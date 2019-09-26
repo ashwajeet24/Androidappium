3cd4da9a8bb705e572b48aee31c5614ef2a834d7
[Ashwajeet] <[ashwajeet.gajre24@gmail.com]>
2019-09-26 02:21:12 -0500

    Initial commit

---
+++ Appium/src/googlepixeldevice/Base.java
@@ -0,0 +1,31 @@
+package googlepixeldevice;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Base {
+
+		public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException 
+		{
+			File f = new File("src");
+			File fs = new File(f,"ApiDemos-debug.apk");
+			
+			DesiredCapabilities cap = new DesiredCapabilities();
+			cap.setCapability(MobileCapabilityType.DEVICE_NAME, "AshwaPhone");
+			cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
+			cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+			
+			AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			return driver;
+		}
+
+}
